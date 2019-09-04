bdd8ca2c15fb6c67a7ebf170b7d3a9ae250992d8
RobinMarien <dev.robinmarien@gmail.com>
2019-09-04 17:02:07 +0200

    end of day

--- script.js
+++ script.js
@@ -1,26 +1,96 @@
+document.getElementById("pscore").innerHTML = window.localStorage.getItem('player_score');
+document.getElementById("cscore").innerHTML = window.localStorage.getItem('computer_score');
 
 
-var weapons = document.querySelectorAll("button");
+var weapons = document.getElementById("buttongroup").querySelectorAll("button");
 
 weapons.forEach((weapon) => {
     weapon.addEventListener('click', () => {
 
         var choice = weapon.getAttribute("id");
         var comp = Math.random();
+        var playerImg = document.getElementById("choice");
+        var compImg = document.getElementById("computer");
+        var result = document.getElementById("result");
+        var pscore = parseInt(document.getElementById("pscore").innerHTML,10);
+        var cscore = parseInt(document.getElementById("cscore").innerHTML,10)
 
-        if (comp < 0.34){
+        playerImg.setAttribute("src", "./img/"+choice+".png");
+
+        if (comp < 0.30){
             comp = "rock";
+            compImg.setAttribute("class", "imgflip");
+            compImg
         }
-        else if (comp < 0.67) {
+        else if (comp < 0.60) {
             comp = "paper";
+            compImg.setAttribute("class", "imgflip");
+        }
+        else if (comp < 0.90){
+            comp = "scissors";
+            compImg.setAttribute("class", "imgflip");
         }
         else{
-            comp = "scissors";
+            comp = "therock";
+            compImg.removeAttribute("class");
         }
-        console.log(comp)
+
+        compImg.setAttribute("src", "./img/"+comp+".png");
+
+        console.log(comp);
 
         if(choice == comp){
-            
+            result.innerText = "It's a Draw"
         }
+        else if (choice == "rock" && comp == "scissors"){
+            result.innerText = "The computer chose Scissors!\nYou win!";
+            pscore++;
+            document.getElementById("pscore").innerText= pscore;
+        }
+        else if (choice == "rock" && comp == "paper"){
+            result.innerText = "The computer chose Paper!\nYou lose!";
+            cscore++;
+            document.getElementById("cscore").innerText= cscore;
+        }
+        else if (choice == "paper" && comp == "rock"){
+            result.innerText = "The computer chose Rock!\nYou win!";
+            pscore++;
+            document.getElementById("pscore").innerText= pscore;
+        }
+        else if (choice == "paper" && comp == "scissors"){
+            result.innerText = "The computer chose Scissors!\nYou lose!";
+            cscore++;
+            document.getElementById("cscore").innerText= cscore;
+        }
+        else if (choice == "scissors" && comp == "paper"){
+            result.innerText = "The computer chose Paper!\nYou win!";
+            pscore++;
+            document.getElementById("pscore").innerText= pscore;
+        }
+        else if (choice == "scissors" && comp == "rock"){
+            result.innerText = "The computer chose Rock!\nYou lose!";
+            cscore++;
+            document.getElementById("cscore").innerText= cscore;
+        }
+        else{
+            result.innerText = "Aww shit, the computer chose THE Rock!!!\nYou can't beat him.. and he demands 3 points";
+            pscore -= 3;
+            cscore += 3;
+            document.getElementById("pscore").innerText= pscore;
+            document.getElementById("cscore").innerText= cscore;
+        }
+
+        if (pscore < 0){
+            pscore = 0;
+            document.getElementById("pscore").innerText= pscore;
+        }
+
+        window.localStorage.setItem('computer_score', JSON.stringify(cscore));
+        window.localStorage.setItem('player_score', JSON.stringify(pscore));
     });
 });
+
+document.getElementById("reset").addEventListener("click", function(){
+    document.getElementById("pscore").innerHTML = "0";
+    document.getElementById("cscore").innerHTML = "0";
+})
